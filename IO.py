c74785398f5511230c284d97c967d0c0cdbe4ef0
ageorge95 <arteni.george.daniel@gmail.com>
2022-06-17 14:16:58 +0300

    added some data type conversions in IO

--- IO.py
+++ IO.py
@@ -22,6 +22,12 @@ class IO_handler():
 
     def save_data(self,
                   object: Dict) -> None:
+
+        # convert the keys (datetime) into strings
+        new_obj = {}
+        for k,v in object.items():
+            new_obj[str(k)] = v
+
         try:
             retry = 0
             while True:
@@ -31,10 +37,11 @@ class IO_handler():
 
                 try:
                     with open(f'{self.server_used}.json', 'w') as json_out_handle:
-                        dump(object,
+                        dump(new_obj,
                              json_out_handle,
                              indent=2)
                     self._log.info(f"Successfully saved {path.abspath(f'{self.server_used}.json')} at retry {retry} !")
+                    break
                 except:
                     self._log.warning(f"Failed to save {path.abspath(f'{self.server_used}.json')} at retry {retry} !\n{format_exc(chain=False)}")
                     sleep(2)
@@ -45,7 +52,15 @@ class IO_handler():
         if path.isfile(f'{self.server_used}.json'):
             try:
                 with open(f'{self.server_used}.json', 'r') as json_in_handle:
-                    return load(json_in_handle)
+                    old_data = load(json_in_handle)
+                    data = {}
+                    # reconvert the str back to datetime
+                    for k, v in old_data.items():
+                        data[datetime.fromisoformat(k)] = v
+
+                    # trim the json if it is really huge (records > 1 week will be removed)
+                    datetime_now = datetime.now()
+                    return dict(filter(lambda _:(datetime_now - _[0]).total_seconds() < 60*60*24*30, data.items()))
             except:
                 self._log.error(f"Failed to load {path.abspath(f'{self.server_used}.json')} !\n{format_exc(chain=False)}")
         else:
