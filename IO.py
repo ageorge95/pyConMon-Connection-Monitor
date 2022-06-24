8c47e621e0448816480f29ada641948e8103cf49
George Daniel Arteni <arteni.george.daniel@gmail.com>
2022-06-24 20:52:41 +0300

    replaced the plot with more relevant data - rectangles also implemented major code optimizations

--- IO.py
+++ IO.py
@@ -1,13 +1,13 @@
-from json import load,\
-    dump
-from typing import Dict,\
-    AnyStr
+from typing import AnyStr,\
+    List
 from logging import getLogger
 from os import path
 from time import sleep
 from traceback import format_exc
 from datetime import datetime
 from InternetAvailability import InternetAvailability
+from pickle import load,\
+    dump
 
 class IO_handler():
 
@@ -21,12 +21,7 @@ class IO_handler():
         self.server_used = server_used
 
     def save_data(self,
-                  object: Dict) -> None:
-
-        # convert the keys (datetime) into strings
-        new_obj = {}
-        for k,v in object.items():
-            new_obj[str(k)] = v
+                  object: List) -> None:
 
         try:
             retry = 0
@@ -36,33 +31,25 @@ class IO_handler():
                     raise Exception('Maximum number of retries reached !')
 
                 try:
-                    with open(f'{self.server_used}.json', 'w') as json_out_handle:
-                        dump(new_obj,
-                             json_out_handle,
-                             indent=2)
-                    self._log.info(f"Successfully saved {path.abspath(f'{self.server_used}.json')} at retry {retry} !")
+                    with open(f'{self.server_used}.pickle', 'wb') as pickle_out_handle:
+                        dump(object,
+                             pickle_out_handle)
+                    self._log.info(f"Successfully saved {path.abspath(f'{self.server_used}.pickle')} at retry {retry} !")
                     break
                 except:
-                    self._log.warning(f"Failed to save {path.abspath(f'{self.server_used}.json')} at retry {retry} !\n{format_exc(chain=False)}")
+                    self._log.warning(f"Failed to save {path.abspath(f'{self.server_used}.pickle')} at retry {retry} !\n{format_exc(chain=False)}")
                     sleep(2)
         except:
-            self._log.error(f"Failed to save {path.abspath(f'{self.server_used}.json')} !")
+            self._log.error(f"Failed to save {path.abspath(f'{self.server_used}.pickle')} !")
 
-    def load_data(self) -> Dict:
-        if path.isfile(f'{self.server_used}.json'):
+    def load_data(self) -> List:
+        if path.isfile(f'{self.server_used}.pickle'):
             try:
-                with open(f'{self.server_used}.json', 'r') as json_in_handle:
-                    old_data = load(json_in_handle)
-                    data = {}
-                    # reconvert the str back to datetime
-                    for k, v in old_data.items():
-                        data[datetime.fromisoformat(k)] = v
-
-                    # trim the json if it is really huge (records > 1 week will be removed)
-                    datetime_now = datetime.now()
-                    return dict(filter(lambda _:(datetime_now - _[0]).total_seconds() < 60*60*24*30, data.items()))
+                with open(f'{self.server_used}.pickle', 'rb') as json_in_handle:
+                    return load(json_in_handle)
             except:
-                self._log.error(f"Failed to load {path.abspath(f'{self.server_used}.json')} !\n{format_exc(chain=False)}")
+                self._log.error(f"Failed to load {path.abspath(f'{self.server_used}.pickle')} !\n{format_exc(chain=False)}")
         else:
-            self._log.warning(f"{path.abspath(f'{self.server_used}.json')} not found. Bootstrapping a new data dict ...")
-            return {datetime.now(): InternetAvailability().check_online_status()}
+            self._log.warning(f"{path.abspath(f'{self.server_used}.pickle')} not found. Bootstrapping a new data dict ...")
+            return [{'date': datetime.now(),
+                     'status': InternetAvailability().check_online_status()}]
